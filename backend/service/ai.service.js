8fa8a3e10db7121726bbcd341bf2948c619223bc
Divyanshi <divyanshi10473@gmail.com>
2025-10-07 13:03:08 +0530

    change

--- backend/service/ai.service.js
+++ backend/service/ai.service.js
@@ -2,7 +2,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 
 
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+  const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
   export const generateResult = async (prompt) => {
 
